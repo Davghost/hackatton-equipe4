827176db0692d52e45dbbabfe2740f65e50ddcb7
Aryane <aryanebueno70@gmail.com>
2025-07-24 18:25:57 -0300

    filtro pronto, falta css do formulario

--- app.py
+++ app.py
@@ -1,15 +1,47 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.get("/")
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.get("/formulario")
+@app.route("/formulario")
 def form():
     return render_template("formulario.html")
 
+@app.route("/cardapio", methods=["POST"])
+def cardapio():
+    restricoes = request.form.getlist("restricoes")
 
-if __name__ == '__main__':
+    pratos = [
+        {
+            "nome": "Feijoada",
+            "imagem": "feijoada.jpg",
+            "restricoes": ["Vegano", "Vegetariano"]
+        },
+        {
+            "nome": "Salada Vegana",
+            "imagem": "salada_vegana.jpg",
+            "restricoes": []
+        },
+        {
+            "nome": "Lasanha Sem Gluten",
+            "imagem": "lasanha.jpg",
+            "restricoes": ["Leite", "Gluten", "Vegano"]
+        },
+        {
+            "nome": "Hambúrguer Vegano",
+            "imagem": "hamburguer_vegano.jpg",
+            "restricoes": ["Gluten"]
+        }
+    ]
+
+    pratos_filtrados = [
+        prato for prato in pratos
+        if not any(r in prato["restricoes"] for r in restricoes)
+    ]
+    return render_template("cardapio.html", pratos=pratos_filtrados, restricoes=restricoes)
+
+if __name__ == "__main__":
     app.run(debug=True)
